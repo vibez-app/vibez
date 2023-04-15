c84af208828c2875e671218089b9fd94f4400cb5
Cristian Corrales <crisdevs95@gmail.com>
2023-04-15 14:55:16 -0400

    finished the arrow functionality

--- client/components/MainPage/Visualizer.jsx
+++ client/components/MainPage/Visualizer.jsx
@@ -4,6 +4,11 @@ import {faChevronLeft, faChevronRight} from '@fortawesome/free-solid-svg-icons';
 import MoodOrb from './MoodOrb';
 import UserContext from '../../UserContext';
 import formatDateString from '../../Utils/formatDateString';
+import getDateString from '../../Utils/getDateString';
+
+
+
+// onClick={console.log(getDateString(userContext.date, "previous"))}
 
 // Downloaded and imported fontawesome so an arrow icon can be used 
 // Used flex box on the button's parent class to center the arrow icon into the box
@@ -13,7 +18,7 @@ function Visualizer() {
 	return (
 		<div className="orbContainer">
 			<div className="backButton flex flex-col justify-center">
-                <button type="button" className="h-[100%] arrow-icon fa-7x"> <FontAwesomeIcon icon={faChevronLeft} className="hover:text-indigo-500 bg-transparent text-white"/></button>
+                <button type="button" onClick={()=>{userContext.changeDate(getDateString(userContext.date, "previous"))}} className="h-[100%] arrow-icon fa-7x"> <FontAwesomeIcon icon={faChevronLeft} className="hover:text-indigo-500 bg-transparent text-white"/></button>
 			</div>
 			<div className="orbVisualizer">
 				<p className="text-white font-bold text-center">
@@ -21,9 +26,9 @@ function Visualizer() {
 				</p>
 				<MoodOrb />
 			</div>
-			<div className="nextButton flex flex-col justify-center">
-                <button type="button" className="h-[100%] fa-7x" > <FontAwesomeIcon icon={faChevronRight} className="hover:text-indigo-500 bg-transparent text-white"/></button>
-                </div>
+			{(userContext.date !== userContext.startDate) && <div className="nextButton flex flex-col justify-center">
+                <button type="button" onClick={()=>{userContext.changeDate(getDateString(userContext.date, "next"))}} className="h-[100%] fa-7x" > <FontAwesomeIcon icon={faChevronRight} className="hover:text-indigo-500 bg-transparent text-white"/></button>
+                </div>}
 		</div>
 	);
 }
